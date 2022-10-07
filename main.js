8a61dcf29f745d59373a1f6fde16108be6779d2d
trippByter <psilocybit5211@gmail.com>
2022-10-07 14:11:43 -0400

    07102022T1411. Uso de ENDPOINTS y QUERY PARAMETERS.

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 console.log("Cargando - Console.log");
 
-const URL = "https://api.thecatapi.com/v1/images/search"
+// ENDPOINT con QUERY PARAM.
+const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3"
 
 
-fetch(URL)
-  // transformamos la promesa a JSON
-  .then(res => res.json())
-  .then(data => {
-    // Tomamos la etiqueta "img" del DOM,
-    // agregamos atrubuto "src" con la url 
-    // de la respueta JSON
-    const img = document.querySelector("img");
-    img.src = data[0].url;
-  });
+async function reload(){
+  // Llamada a la API
+  const res = await fetch(API_URL);
+  // Pasamos respuesta a JSON
+  const data = await res.json();
+  
+  console.log(data);
+  // Manipulación DOM
+  const img00 = document.getElementById("img00");
+  const img01 = document.getElementById("img01");
+  const img02 = document.getElementById("img02");
+  // Cambiamos el atributo "src"
+  img00.src = data[0].url;
+  img01.src = data[1].url;
+  img02.src = data[2].url;
+};
+
+reload();
