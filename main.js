a4fb5f3a71734a03bbe8e8efceca177f032a7991
trippByter <psilocybit5211@gmail.com>
2022-10-08 00:30:53 -0400

    08102022T0030 Manejo de errores.

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
-console.log("Cargando - Console.log");
-
 // ENDPOINT con QUERY PARAM.
-const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3";
+const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=2";
+const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?limit=2";
 
+const spanError = document.getElementById("error");
 
-async function reload(){
+async function loadRandomCats(){
   // Llamada a la API
-  const res = await fetch(API_URL);
+  const res = await fetch(API_URL_RANDOM);
   // Pasamos respuesta a JSON
   const data = await res.json();
   
+  if(res.status !== 200){
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  }else{
+    // Manipulación DOM
+    const img00 = document.getElementById("img00");
+    const img01 = document.getElementById("img01");
+    // Cambiamos el atributo "src"
+    img00.src = data[0].url;
+    img01.src = data[1].url;
+  }
+
+  console.log("_=_=_=_=_RANDOM_=_=_=_=_");
   console.log(data);
-  // Manipulación DOM
-  const img00 = document.getElementById("img00");
-  const img01 = document.getElementById("img01");
-  const img02 = document.getElementById("img02");
-  // Cambiamos el atributo "src"
-  img00.src = data[0].url;
-  img01.src = data[1].url;
-  img02.src = data[2].url;
+  console.log("_=_=_=_=_END_RANDOM_=_=_=_=_");  
 };
 
-reload();
+// Favorites in HTML
+async function loadFavoritesCats(){
+  // Llamada a la API
+  const res = await fetch(API_URL_FAVORITES);
+  // Pasamos respuesta a JSON
+  // const data = await res.json();
+
+  // console.log("_=_=_=_=FAVORITOS=_=_=_=_");
+  // console.log(data);
+  // console.log("_=_=_=_=END FAVORITOS=_=_=_=_");
+
+  if(res.status !== 200){
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  };
+}
+
+
+loadRandomCats();
+loadFavoritesCats();
