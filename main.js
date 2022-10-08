48b0790f0e64f5f7dda5586eda3733bc60a74bdc
trippByter <psilocybit5211@gmail.com>
2022-10-08 12:44:51 -0400

    08102022T1244_MetodoPOST: Guardando gatos en favoritos.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // ENDPOINT con QUERY PARAM.
 const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=2";
-const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?limit=2";
+const API_URL_FAVOURITES = "https://api.thecatapi.com/v1/favourites?api_key=live_XxJk9btOENRbvt13t6onIYMHUviZN2H6F9eu1DzZkzfZnHDMnP8Pdda4fUvXyleN";
 
 const spanError = document.getElementById("error");
 
@@ -26,22 +26,47 @@ async function loadRandomCats(){
   console.log("_=_=_=_=_END_RANDOM_=_=_=_=_");  
 };
 
-// Favorites in HTML
-async function loadFavoritesCats(){
+// Favorites en HTML
+async function loadFavouritesCats(){
   // Llamada a la API
-  const res = await fetch(API_URL_FAVORITES);
+  const res = await fetch(API_URL_FAVOURITES);
   // Pasamos respuesta a JSON
-  // const data = await res.json();
+  const data = await res.json();
 
-  // console.log("_=_=_=_=FAVORITOS=_=_=_=_");
-  // console.log(data);
-  // console.log("_=_=_=_=END FAVORITOS=_=_=_=_");
+  console.log("_=_=_=_=FAVORITOS=_=_=_=_");
+  console.log(data);
+  console.log("_=_=_=_=END FAVORITOS=_=_=_=_");
 
   if(res.status !== 200){
-    spanError.innerHTML = "Hubo un error: " + res.status;
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
   };
 }
 
+// la documentación indica que para guardar
+// "favoritos", hay que usar método POST
+// body: JSON("image_id") - Mandamos un str
+// porque no sabemos en que lenguaje está escrito
+// backend. 
+async function saveFavouritesCats(){
+  const res = await fetch(API_URL_FAVOURITES, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      image_id: "2ke"
+    })
+  });
+  
+  const data = await res.json();
+  // console.log(res);
+  console.log(data);
+
+  if(res.status !== 200){
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  }
+};
+
 
 loadRandomCats();
-loadFavoritesCats();
+loadFavouritesCats();
